755906eab5656a48753e2b8fea50b33bb4428916
Andrew Hibbard <andrew.o.hibbard@gmail.com>
2020-03-11 19:14:47 -0700

    in the middle of async fetch issue

--- frontend/src/TeamAdapter.js
+++ frontend/src/TeamAdapter.js
@@ -15,39 +15,40 @@ class TeamAdapter{
         .then(() => console.log(Team.all))
     }
 
-    static fetchGivenTeam(slug){
-        const teamsURL = "http://localhost:3000/teams/";
+    // static fetchGivenTeam(slug){
+    //     const teamsURL = "http://localhost:3000/teams/";
 
-        fetch(teamsURL + slug)
-        .then(res => res.json())
-        .then(team => {
-            let parsed = {id: team.data.id, ...team.data.attributes}
-            let teamObj = new Team(parsed)
-            teamObj.tasks.push(team.data.attributes.tasks)
-            renderTeam(teamObj)
-        })
-        //.then(() => console.log(this.team))
-        //.then(() => renderTeam(team))
-    }
+    //     fetch(teamsURL + slug)
+    //     .then(res => res.json())
+    //     .then(team => {
+    //         let parsed = {id: team.data.id, ...team.data.attributes}
+    //         let teamObj = new Team(parsed)
+    //         teamObj.tasks.push(team.data.attributes.tasks)
+    //         teamObj.renderTeam();
+    //     })
+    //     //.then(() => console.log(this.team))
+    //     //.then(() => renderTeam(team))
+    // }
 
-    renderTeam(){
-        const teamField = document.querySelector('#team-container')
-        let teamTasks = createTaskField(this.team.tasks)
-        teamField.innerHTML += `<div class="team-display" data-id="${this.team.id}">
-            <h2>${this.team.name}</h2>
-            <h2>Our Tasks</h2>
-            <ul class="team-tasks-${this.team.id}" data-id="${this.team.id}">
-                ${teamTasks}
-            </ul>
-        </div>`
-    }
+    // // make static
+    // renderTeam(team){
+    //     const teamField = document.querySelector('#team-container')
+    //     let teamTasks = createTaskField(team.tasks)
+    //     teamField.innerHTML += `<div class="team-display" data-id="${team.id}">
+    //         <h2>${team.name}</h2>
+    //         <h2>Our Tasks</h2>
+    //         <ul class="team-tasks-${team.id}" data-id="${team.id}">
+    //             ${teamTasks}
+    //         </ul>
+    //     </div>`
+    // }
 
-    createTaskField(){
-        let teamTasks = '';
-        for (const task of this.taskList){
-            teamTasks += createIndividualTask(this.task)
-        };
-        return teamTasks;
-    }
+    // static createTaskField(){
+    //     let teamTasks = '';
+    //     for (const task of taskList){
+    //         teamTasks += createIndividualTask(task)
+    //     };
+    //     return teamTasks;
+    // }
 
 }
--- frontend/src/oo-index.js
+++ frontend/src/oo-index.js
@@ -23,9 +23,59 @@ function querySpecificTeam(e){
     let teamObj = Team.all.find(team => team.name === searchInput)
     if (!!teamObj){
         //teamObj.teamAdapter.fetchGivenTeam();
-        TeamAdapter.fetchGivenTeam(teamObj.slug)
+        fetchGivenTeam(teamObj.slug)
     } else {
         console.log("Team does not exist")
     }
 };
+
+function fetchGivenTeam(slug){
+    const teamsURL = "http://localhost:3000/teams/";
+
+    fetch(teamsURL + slug)
+    .then(res => res.json())
+    .then(team => {
+        let parsed = {id: team.data.id, ...team.data.attributes}
+        let teamObj = new Team(parsed)
+
+        teamObj.tasks.push(team.data.attributes.tasks)
+        renderTeam(teamObj);
+    })
+    //.then(() => console.log(this.team))
+    //.then(() => renderTeam(team))
+}
+
+function renderTeam(team){
+    const teamField = document.querySelector('#team-container')
+
+    let teamTasks = team.tasks.forEach(createTaskField);    
+    teamField.innerHTML = `<div class="team-display" data-id="${team.id}">
+        <h2>${team.name}</h2>
+        <h2>Our Tasks</h2>
+        <ul class="team-tasks-${team.id}" id="task-field" data-id="${team.id}">
+        <div>${teamTasks}</div>
+        </ul>
+    </div>`
+    debugger
+}
+
+function createTaskField(teamTaskArr){
+    const taskField = document.querySelector("#task-field");
+    let teamTasks = '';
+    for (const task of teamTaskArr){
+        teamTasks += createIndividualTask(task)
+    };
+}
+
+function createIndividualTask(task){
+    return `<div class="task" task-data-id="${task.id}">
+        <li>${task.title}</li>
+        <li>Due: ${task.due_date}</li>
+        <li>Urgency: ${task.urgent}</li>
+        <li>Notes: ${task.description}<li>
+        <button class="complete" data-id="${task.id}">Complete?</button>
+        <button class ="delete-tasks" data-id="${task.id}">Delete</button>
+        <br>
+    </div><br>` ; 
+}
 
